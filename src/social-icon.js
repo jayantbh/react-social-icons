38ff701329a6a89d0c54dbdac0abc2b05cea796f
Jayant Bhawal <bhawal.jayant@gmail.com>
2019-12-08 16:44:56 +0530

    Change existing icon component to a different name, and add a new SVG only icon component.

--- src/social-icon.js
+++ src/social-icon.js
@@ -5,32 +5,21 @@ import Background from './background.js'
 import Icon from './icon.js'
 import Mask from './mask.js'
 import { keyFor } from './networks.js'
-import { socialIcon, socialContainer, socialSvg } from './styles.js'
 
 function getNetworkKey(props) {
   return props.network || keyFor(props.url)
 }
 
 function SocialIcon(props) {
-  const { url, network, bgColor, fgColor, className, label, ...rest } = props
+  const { url, network, bgColor, fgColor, className } = props
   const networkKey = getNetworkKey({ url, network })
 
   return (
-    <a
-      {...rest}
-      href={url}
-      className={'social-icon' + (!!className ? ' ' + className : '')}
-      style={{ ...socialIcon, ...props.style }}
-      aria-label={label || networkKey}
-    >
-      <div className="social-container" style={socialContainer}>
-        <svg className="social-svg" style={socialSvg} viewBox="0 0 64 64">
-          <Background />
-          <Icon networkKey={networkKey} fgColor={fgColor} />
-          <Mask networkKey={networkKey} bgColor={bgColor} />
-        </svg>
-      </div>
-    </a>
+    <svg className={`social-svg ${className}`} style={{ borderRadius: '50%' }} viewBox="0 0 64 64">
+      <Background />
+      <Icon networkKey={networkKey} fgColor={fgColor} />
+      <Mask networkKey={networkKey} bgColor={bgColor} />
+    </svg>
   )
 }
 
